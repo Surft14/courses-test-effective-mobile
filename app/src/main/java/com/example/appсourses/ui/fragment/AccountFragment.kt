9187fc568271eaaaa287d8500385200c881a4898
SURFT!$ <mr.darck31@gmail.com>
2025-07-11 15:46:59 +0200

    Created CourseFragment.kt

--- app/src/main/java/com/example/appсourses/ui/fragment/AccountFragment.kt
+++ app/src/main/java/com/example/appсourses/ui/fragment/AccountFragment.kt
@@ -1,13 +1,22 @@
 package com.example.appcourses.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.appcourses.R
 
 class AccountFragment : Fragment() {
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
--- app/src/main/java/com/example/appсourses/ui/fragment/CourseFragment.kt
+++ app/src/main/java/com/example/appсourses/ui/fragment/CourseFragment.kt
@@ -1,13 +1,42 @@
 package com.example.appсourses.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.appcourses.R
+import com.example.appcourses.databinding.FragmentCourseBinding
+import com.example.appсourses.viewmodel.CourseViewModel
+import com.example.appсourses.viewmodel.MainScreenViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class CourseFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentCourseBinding
+    private val courseViewModel: CourseViewModel by viewModel()
+    private val screenViewModel: MainScreenViewModel by viewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCourseBinding.inflate(layoutInflater)
+
+        val course = courseViewModel.selectedCourse.value
+
+        binding.bFavorite.setOnClickListener {
+            val course = courseViewModel.selectedCourse
+            course.value?.hasLike = !course.value!!.hasLike
+        }
+
+        binding.bBack.setOnClickListener {
+            screenViewModel.itemSelected(MainScreenViewModel.Screen.HOME)
+        }
+
+        binding.tvTextF.text = course?.text
+        binding.tvTitleCourseF.text = course?.title
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
